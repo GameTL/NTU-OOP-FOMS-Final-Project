c310b019331435bbdfae6dcd9dd8b8ce07cfefc0
tracehhh <tracehheso@gmail.com>
2024-04-02 20:30:08 +0800

    test

--- Menu.java
+++ Menu.java
@@ -5,6 +5,27 @@ public class Menu {
 
     public Menu(){
         this.items = new ArrayList<>();
+        initializeMenuItems();
+        sortItemsByCategory();
+    }
+    private void initializeMenuItems(){
+        // Initialization for NTU branch
+        items.add(new MenuItem("FRIES", 3.2, "NTU", "Side"));
+        items.add(new MenuItem("3PC SET MEAL", 9.9, "NTU", "Set meal"));
+        items.add(new MenuItem("COLE SLAW", 2.7, "NTU", "Side"));
+        items.add(new MenuItem("CHICKEN NUGGET", 6.6, "NTU", "Side"));
+
+        // Initialization for JP branch
+        items.add(new MenuItem("CAJUN FISH", 5.6, "JP", "Burger"));
+        items.add(new MenuItem("CHICKEN NUGGET", 6.9, "JP", "Side"));
+
+        // Initialization for JE branch
+        items.add(new MenuItem("COLE SLAW", 2.7, "JE", "Side"));
+        items.add(new MenuItem("3PC SET MEAL", 10.4, "JE", "Set meal"));
+        items.add(new MenuItem("PEPSI", 2.1, "JE", "Drink"));
+    }
+    private void sortItemsByCategory(){
+        items.sort(Comparator.comparing(MenuItem::getCategory));
     }
     public void addMenuItem(MenuItem item) {
         items.add(item);
@@ -21,4 +42,17 @@ public class Menu {
                 }
         }
     }
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please select a branch (NTU, JP, or JE): ");
+        String branch = scanner.nextLine();
+
+        menu.setBranch();
+        menu.displaymenu();
+        
+    }
 }
+
+
