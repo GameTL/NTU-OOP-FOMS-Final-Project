fef2380fe34517e79c7a0bc9846369e86bb023fc
Game Limsila <limsila.limsila@yahoo.com>
2024-04-03 12:47:14 +0800

    added and move some stuff

---
+++ fomsOperations.java
@@ -0,0 +1,43 @@
+public interface fomsOperations {
+    // Main
+    void userSelector();
+    // Level 1
+
+    // >Staff
+    void adminHome();
+
+    void editStaff();
+
+    void staffLogin();
+
+    void staffHome();
+
+    // >>Admin
+    void assignManager();
+
+    void cardPaymentGateway();
+
+    void displayStaff();
+
+    void editPayment();
+
+    void openCloseBranch();
+
+    void promoteStaff();
+
+    void transferStaff();
+
+    // >>BranchManager
+    void editMenu();
+
+    void displayBranchStaff();
+
+    // >User
+    void branchSelector();
+
+    void displayCurrentOrder();
+
+    void menuList();
+    // Level X.X.1
+
+}
